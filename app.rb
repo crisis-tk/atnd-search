02b47fad6f436d76eb8c414d029823a6ee7429da
kouta <kouta@leopard.local>
2012-03-24 13:51:06 +0900

    atnd search

---
+++ app.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'open-uri'
+require 'json'
+
+configure :production do
+end
+
+get '/' do
+  haml :index
+end
+
+post '/result' do
+  keyword = params[:keyword]
+  request_url = "http://api.atnd.org/events/?format=json&keyword=#{keyword}"
+
+  uri = URI.encode(request_url)
+
+  json   = open(uri).read
+  result = JSON.parse(json)
+
+  @events = result['events'].collect {|event| event['title']}
+
+  haml :result
+end
+
+
