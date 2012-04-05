8acf97288243a01b250d2ac991a80a34b590f6db
Kouta TOUME <ahagonn2000@gmail.com>
2012-04-05 22:29:21 +0900

    added zusaar search

--- app.rb
+++ app.rb
@@ -13,17 +13,26 @@ get '/' do
   haml :index
 end
 
+def request_to(url)
+  uri = URI.encode(url)
+  json = open(uri).read
+  result = JSON.parse(json)
+  return result
+end
+
 post '/result' do
   keyword = params[:keyword]
   keyword = keyword.gsub!(' ', ',') if keyword.include?(' ')
-  request_url = "http://api.atnd.org/events/?format=json&keyword=#{keyword}"
 
-  uri = URI.encode(request_url)
+  # ATND検索
+  atnd_api = "http://api.atnd.org/events/?format=json&keyword=#{keyword}"
+  atnd_result = request_to(atnd_api)
+  @atnd_events = atnd_result['events'].collect {|event| event['title']}
 
-  json   = open(uri).read
-  result = JSON.parse(json)
-
-  @events = result['events'].collect {|event| event['title']}
+  # Zusaar検索
+  zusaar_api = "http://www.zusaar.com/api/event/?format=json&keyword=#{keyword}"
+  zusaar_result  = request_to(zusaar_api)
+  @zusaar_events = zusaar_result['event'].collect {|event| event['title'] }
 
   haml :result
 end
